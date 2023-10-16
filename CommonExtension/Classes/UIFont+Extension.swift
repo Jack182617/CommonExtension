3e9f9000cead0286a121fc840173d31fd429f75e
Jack <550936272@qq.com>
2023-10-16 15:13:55 +0800

    1.0.7submit

--- CommonExtension/Classes/UIFont+Extension.swift
+++ CommonExtension/Classes/UIFont+Extension.swift
@@ -9,41 +9,60 @@ import Foundation
 
 public extension UIFont {
     class func font(_ size: CGFloat) -> UIFont { .systemFont(ofSize: size) }
+    
+    class func ultraLight(_ size: CGFloat) -> UIFont { .systemFont(ofSize: size, weight: .ultraLight) }
+    
+    class func thin(_ size: CGFloat) -> UIFont { .systemFont(ofSize: size, weight: .thin) }
+    
+    class func light(_ size: CGFloat) -> UIFont { .systemFont(ofSize: size, weight: .light) }
+    
     class func regular(_ size: CGFloat) -> UIFont { .systemFont(ofSize: size, weight: .regular) }
+    
     class func medium(_ size: CGFloat) -> UIFont { .systemFont(ofSize: size, weight: .medium) }
+    
     class func semibold(_ size: CGFloat) -> UIFont { .systemFont(ofSize: size, weight: .semibold) }
+    
+    class func bold(_ size: CGFloat) -> UIFont { .systemFont(ofSize: size, weight: .bold) }
+    
+    class func heavy(_ size: CGFloat) -> UIFont { .systemFont(ofSize: size, weight: .heavy) }
+    
+    class func black(_ size: CGFloat) -> UIFont { .systemFont(ofSize: size, weight: .black) }
 
     // PingFangSC
-    class func pfMedium(_ size: CGFloat) -> UIFont {
-        UIFont(name: "PingFangSC-Medium", size: size) ?? .font(size)
+    class func pfUltralight(_ size: CGFloat) -> UIFont {
+        UIFont(name: "PingFangSC-Ultralight", size: size) ?? .ultraLight(size)
     }
-
-    class func pfRegular(_ size: CGFloat) -> UIFont {
-        UIFont(name: "PingFangSC-Regular", size: size) ?? .font(size)
+    
+    class func pfThin(_ size: CGFloat) -> UIFont {
+        UIFont(name: "PingFangSC-Thin", size: size) ?? .thin(size)
     }
-
+    
     class func pfLight(_ size: CGFloat) -> UIFont {
-        UIFont(name: "PingFangSC-Light", size: size) ?? .font(size)
+        UIFont(name: "PingFangSC-Light", size: size) ?? .light(size)
+    }
+    
+    class func pfRegular(_ size: CGFloat) -> UIFont {
+        UIFont(name: "PingFangSC-Regular", size: size) ?? .regular(size)
+    }
+    
+    class func pfMedium(_ size: CGFloat) -> UIFont {
+        UIFont(name: "PingFangSC-Medium", size: size) ?? .medium(size)
     }
 
     class func pfSemibold(_ size: CGFloat) -> UIFont {
-        UIFont(name: "PingFangSC-Semibold", size: size) ?? .font(size)
+        UIFont(name: "PingFangSC-Semibold", size: size) ?? .semibold(size)
+    }
+    
+    class func pfBold(_ size: CGFloat) -> UIFont {
+        UIFont(name: "PingFangSC-Bold", size: size) ?? .bold(size)
+    }
+    
+    class func pfHeavy(_ size: CGFloat) -> UIFont {
+        UIFont(name: "PingFangSC-Heavy", size: size) ?? .heavy(size)
+    }
+    
+    class func pfBlack(_ size: CGFloat) -> UIFont {
+        UIFont(name: "PingFangSC-Black", size: size) ?? .black(size)
     }
 
-    // Arial
-    class func arialMT(_ size: CGFloat) -> UIFont {
-        UIFont(name: "ArialMT", size: size) ?? .font(size)
-    }
-
-    class func arialItalicMT(_ size: CGFloat) -> UIFont {
-        UIFont(name: "Arial-ItalicMT", size: size) ?? .font(size)
-    }
-
-    class func arialBoldMT(_ size: CGFloat) -> UIFont {
-        UIFont(name: "Arial-BoldMT", size: size) ?? .font(size)
-    }
-
-    class func arialBoldItalicMT(_ size: CGFloat) -> UIFont {
-        UIFont(name: "Arial-BoldItalicMT", size: size) ?? .font(size)
-    }
 }
